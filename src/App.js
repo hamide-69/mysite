99f6e4cfbf9409cd6009968005587952a7e70005
Hamide <mohammadi.sut@gmail.com>
2020-02-25 11:52:26 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from './About'
+import Contact from './Contact'
+import Nav from './Nav'
+import AddTxt from './AddTxt'
+import Slider  from './Slider'
+import Gallery from './Gallery'
+import Login from './Login'
+import Logout from './Logout'
+import Admin from './Admin'
+import ComplexList from './ComplexList'
+import Uplode from './Uplode'
+import Portfolio from './Portfolio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import './App.css'
+
+
+function App(){
+   return(
+      <Router className="container-fluid" >
+      <div className='App row'>
+<Nav  />
+<div  className="col-md-9">
+<switch >
+<Route path="/" exact component={Home}/>
+<Route path="/home"  component={Home}/>
+<Route path="/about" component={About}/>
+<Route path="/portfolio" component={Portfolio}/>
+<Route path="/gallery" component={Gallery}/>
+<Route path="/addtxt" component={AddTxt}/>
+<Route path="/ComplexList" component={ComplexList}/>
+<Route path="/contact" component={Contact}/>
+<Route path="/login" component={Login}/>
+<Route path="/admin" component={Admin}/>
+<Route path="/logout" component={Logout}/>
+<Route path="/uplode" component={Uplode}/>
+
+
+
+
+</switch>
+
+</div>
+        </div> 
+        </Router>
+
+   )
 }
 
-export default App;
+
+const Home = () => (
+   <div>
+  
+  <Slider  />
+   
+
+   </div>
+);
+
+export default App
